53364c00b2a5bfcf760416c2bfc337946500a590
Ingvar <shymanski.ihar@gmail.com>
2018-10-12 18:57:52 +0300

    Update MainKeyboard/MainKeyboard.ino
    Not so good

--- MainKeyboard/MainKeyboard.ino
+++ MainKeyboard/MainKeyboard.ino
@@ -1,63 +1,50 @@
-#include <Keyboard.h>
+#include <TimerThree.h>
 #include <RCSwitch.h>
-#include <MsTimer2.h>
+#include <Keyboard.h>
 
 const byte LOWVOLTAGE = bit(2);
 
 RCSwitch mySwitch = RCSwitch();
 bool activeButtons[4] = {true, true, true, true};
 const byte id = 0;
-bool isPushed = false;
 
 void setup() {
- 
-  Serial.begin(9600);
   Keyboard.begin();
   pinMode(3, INPUT_PULLUP);
   
-  mySwitch.enableReceive(0);
+  mySwitch.enableReceive(1);
 
-  attachInterrupt(1, OnPushButton, FALLING);
-  MsTimer2::set(1000, Timer);
-  MsTimer2::start();
+  Timer3.initialize(1000000);
+  Timer3.attachInterrupt(Timer);
+  Timer3.start();
 }
-
+//22
 void loop() {
-  if (isPushed){
-    delay(50);
-  }
-  
-  ButtonsDataHandle();
 
-  if (isPushed) {
+ ButtonsDataHandle();
+
+  if (activeButtons[0] && digitalRead(3) == LOW) {
     Keyboard.print('0');
-    isPushed = false;
+    activeButtons[0] = false;
   }
 }
 
 void ButtonsDataHandle(){
    if ( mySwitch.available() ) {
     byte value = mySwitch.getReceivedValue();
-    byte senderId = value & 3;
+    char senderId = (value & 3) + '0';
     switch (value & 252) {
       case LOWVOLTAGE:
-        Keyboard.print(String(senderId) + "l");
+        Keyboard.print("l" + senderId);
         break;
       case 0:
-        if (activeButtons[senderId]) {
-          Keyboard.print(senderId + '0');
-          activeButtons[senderId] = false;
+        if (activeButtons[senderId-'0']) {
+          Keyboard.print(senderId);
+          activeButtons[senderId-'0'] = false;
         }
         break;
     }
     mySwitch.resetAvailable();
-  }
-}
-
-void OnPushButton() {
-  if (activeButtons[0]) {
-    isPushed = true;
-    activeButtons[0] = false;
   }
 }
 
